7a32bf235f577a995381dbcea8c3883b78099ae6
glebite <glebite@gmail.com>
2021-07-23 20:28:12 -0400

    Sigh - silly mistakes are the worst.

--- src/acronyms.py
+++ src/acronyms.py
@@ -2,10 +2,8 @@
 """
 """
 import textract
-from collections import Counter
-# from wordcloud import STOPWORDS
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+# from nltk.tokenize import word_tokenize
 import re
 # import sys
 
@@ -15,12 +13,11 @@ def main(arguments):
 
 
 if __name__ == "__main__":
-    textContents = re.findall(r'\w+',
+    textContents = re.findall(r'\w+|(\(\w+\))+',
                               textract.process('../data/rfc3261.txt.pdf',
-                                                   method='pdfminer').decode("utf8"))
+                                               method='pdfminer').decode("utf8"))
     stop_words = set(stopwords.words('english'))
 
-    c = Counter()
     for word in textContents:
-        if re.match(r"\([A-Z]*\)", word):
+        if '(' in word:
             print(word)
