1613e67c9c5b9f3fb6e24decfca85ca6c36c9a1b
glebite <glebite@gmail.com>
2021-07-24 15:06:47 -0400

    Frustrated with trying to get it to DWIM.

--- src/acronyms.py
+++ src/acronyms.py
@@ -3,8 +3,9 @@
 """
 import textract
 from nltk.corpus import stopwords
-# from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize
 import re
+import copy
 # import sys
 
 
@@ -13,11 +14,19 @@ def main(arguments):
 
 
 if __name__ == "__main__":
-    textContents = re.findall(r'\w+|(\(\w+\))+',
-                              textract.process('../data/rfc3261.txt.pdf',
-                                               method='pdfminer').decode("utf8"))
+    textContents = textract.process('../data/rfc3261.txt.pdf',
+                         method='pdfminer').decode("utf8")
     stop_words = set(stopwords.words('english'))
 
-    for word in textContents:
-        if '(' in word:
-            print(word)
+    print(textContents)
+    
+    # words = word_tokenize(textContents)
+
+    # for pos, word in enumerate(words):
+    #     # print(word)
+    #     # print(f'Pos: {pos} word: {word}')
+    #     if '(' in word:
+    #         print(f'\tBird: {word} {words[pos+1]} {words[pos+2]}')
+    #     # if '(' in word:
+    #     #     print(word)
+    #     #     # print(f'\t{pos} >>{x[0][pos-1]}<<')
