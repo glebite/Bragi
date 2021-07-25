83110059a364babcfab8a379158c3192f677c3dc
glebite <glebite@gmail.com>
2021-07-24 22:15:12 -0400

    Continuing improvement - removed PEP8 violations.

--- src/acronyms.py
+++ src/acronyms.py
@@ -2,11 +2,10 @@
 """
 """
 import textract
-from nltk.corpus import stopwords
-from nltk.tokenize import TreebankWordTokenizer
+# from nltk.corpus import stopwords
+# from nltk.tokenize import TreebankWordTokenizer
 
 import re
-import copy
 # import sys
 
 
@@ -16,9 +15,11 @@ def main(arguments):
 
 if __name__ == "__main__":
     textContents = textract.process('../data/rfc3261.txt.pdf',
-                         method='pdfminer').decode("utf8").split(" ")
+                                    method='pdfminer').decode('utf8')
+    textContents = textContents.split(' ')
     reducedContents = [word for word in textContents if len(word)]
     for pos, word in enumerate(reducedContents):
         if re.search(r'\([A-Z]*\)', word):
             wordLength = len(word)-3
-            print(f'ACRONYM: {word} Definition: {reducedContents[pos-wordLength:pos]}')
+            print(f'ACRONYM: {word} Definition:'
+                  ' {reducedContents[pos-wordLength:pos]}')
