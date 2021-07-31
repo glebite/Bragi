b95dfdd8a2287e2a7c630002ed00050d07aa0664
glebite <glebite@gmail.com>
2021-07-31 11:08:08 -0400

    Updating with htmlgen and tests to go along with its development.

---
+++ src/htmlgen.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+"""
+
+
+class HtmlGen(object):
+    """
+    """
+    def __init__(self):
+        """
+        """
+        pass
+    
+
+if __name__ == "__main__":
+    pass
---
+++ test/test_htmlgen.py
@@ -0,0 +1,7 @@
+from htmlgen import HtmlGen
+
+
+def test_creation():
+    x = HtmlGen()
+    assert x is not None
+
